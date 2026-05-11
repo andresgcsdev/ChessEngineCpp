d5250023a2273bd937fa8d553d4bd1483f60caff
andre <andredesenvolvimentosis@gmail.com>
2026-05-11 02:33:29 -0300

    Fixed depth comments

--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,8 @@ int main()
             // Change this color value to WHITE to play as black.
             if (g.getTurn() != playerColor)
             {
-                // Depth higher than 3 is too slow as of now. Will implement better search in the future.
-                // Depth lower than 3 is instant, but plays pretty badly.
+                // Depth higher than 4 is too slow as of now. Will implement better search in the future.
+                // Depth lower than 4 is instant, but plays pretty badly.
                 std::array<Coord, 2> bestMove = engine.getBestMove(g);
                 g.makeMove(bestMove[0], bestMove[1]);
             }
